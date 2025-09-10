fd585326ae9d3d686cbd9dd57f78d0d2701c42a7
diggens42 <frederic.marcel.wahl@gmail.com>
2025-09-10 03:58:27 +0200

    cracked 4am parsing tech

--- src/parser.c
+++ src/parser.c
@@ -6,7 +6,7 @@
 /*   By: fwahl <fwahl@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/10 01:15:51 by fwahl             #+#    #+#             */
-/*   Updated: 2025/09/10 03:21:00 by fwahl            ###   ########.fr       */
+/*   Updated: 2025/09/10 03:57:05 by fwahl            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,9 +51,38 @@ static const t_opt_def    opt_table[] =
     {NULL, NULL, OPT_NO_ARG, NULL, NULL} // Sentinel
 };
 
-static const *t_opt_def   lookup_opt(const char *arg)
+static char *get_opt_val(const char *arg)
 {
+    char *sep = ft_strchr(arg, '=');
+    return (sep ? (sep + 1) : NULL);
+}
+
+static const t_opt_def   *get_opt_def(const char *arg)
+{
+    char    *val = NULL;
+    char    *opt = (char *)arg;
+
+    if (arg[0] == '-' && arg[1] == '-' && (val = get_opt_val(arg))
+    {
+        size_t  opt_len = opt_val - arg - 1; // -1 for = separator
+        opt = ft_strndup(arg, opt_len);
+        if (!opt)
+            return (NULL);
+    }
     
+    for (const t_opt_def *opt_def = opt_table; opt_def->long_opt || opt_def->short_opt; opt_def++)
+    {
+        if (opt_def->short_opt && ft_strcmp(opt, opt_def->short_opt == 0) 
+            || opt_def->long_opt && ft_strcmp(opt, opt_def->long_opt) == 0)
+        {
+            if (opt != arg)
+                free(opt);
+            return (opt)
+        }
+    }
+    
+    if (opt != arg)
+        free(opt);
     return (NULL);
 }
 
@@ -63,7 +92,7 @@ bool    parse(int argc, char **argv, t_conf *conf)
     {
         if (argv[i][0] == '-')
         {
-            const t_opt_def *opt = lookup_opt(argv[i]);
+            const t_opt_def *opt = get_opt_def(argv[i]);
             if (!parse)
             {
                 fprintf(stderr, "ft_ping: invalid option -- '%s'\n", argv[i]);
