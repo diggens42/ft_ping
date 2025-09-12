9ac6201147f1407be15176bb10607c804923cb8e
diggens42 <frederic.marcel.wahl@gmail.com>
2025-09-13 01:43:03 +0200

    add flags & opts to opt_def table in parser, parse_size(), parse_timeout(), parse_linger()

--- include/structs.h
+++ include/structs.h
@@ -6,7 +6,7 @@
 /*   By: fwahl <fwahl@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/06 15:05:29 by fwahl             #+#    #+#             */
-/*   Updated: 2025/09/13 00:28:50 by fwahl            ###   ########.fr       */
+/*   Updated: 2025/09/13 01:34:56 by fwahl            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,22 @@ typedef struct s_opts
 {
     double              interval;               // -i / --interval flag --> interval of seconds in between packages sent
     uint32_t            count;                  // -c / --count=N flag value --> num of echo req to send 
-    uint32_t            timeout;                // timeout for reply (in ms?)
+    uint32_t            timeout;                // -w timeout in seconds
     uint32_t            linger;                 // -W: per packet timeout in secs
-    uint16_t            packet_size;            // size of ICMP payloads in bytes
+    uint16_t            packet_size;            // -s size of ICMP payloads in bytes
     uint8_t             packet_type;            // -t / --type=TYPE flag -->look notes for ICMP types
+    uint8_t             pattern[16];            // -p payload pattern
+    uint8_t             pattern_len;            // 0-16
     uint8_t             ttl;                    // --ttl flag value --> time to live
+    uint8_t             tos;                    // -T type of service
 }   t_opts;
 
 typedef struct s_conf
 {
     struct sockaddr_in  dest;                   // dest address -->  sendto()
-    char                *tar;                   // hostname
+    char                *tar;                   // original target argument
     char                res_ip[INET_ADDRSTRLEN];// IP after DNS lookup
+    char                hostname[256];          // resolved hostname
     pid_t               pid;                    // PID for ICMP identifier
     int                 socket_fd;              // raw socket fd
     uint8_t             flags;                  // bitmask instead of bool flag struct
@@ -70,7 +74,7 @@ typedef struct s_opt_def
 {
     const char  *short_opt;     // e.g., "-v" (NULL if no short form)
     const char  *long_opt;      // e.g., "--verbose"
-    t_opt_type  type;         // OPT_NO_ARG or OPT_REQUIRED_ARG
+    bool        has_arg;         // OPT_NO_ARG or OPT_REQUIRED_ARG
     uint8_t     flag_bit;      // which flag to set, 0 means not a flag
     int         (*parser)(const char *val, t_conf *conf);  // Custom parser function
 }   t_opt_def;
--- src/parser.c
+++ src/parser.c
@@ -6,11 +6,60 @@
 /*   By: fwahl <fwahl@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/10 01:15:51 by fwahl             #+#    #+#             */
-/*   Updated: 2025/09/13 00:32:05 by fwahl            ###   ########.fr       */
+/*   Updated: 2025/09/13 01:39:38 by fwahl            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/ft_ping.h"
+
+static int parse_size(const char *val, t_conf *conf)
+{
+    char *endptr;
+    long n;
+    
+    errno = 0;
+    n = ft_strtol(val, &endptr, 10);
+    // ICMP data size limits: 8 bytes min (timestamp), 65507 max (IP limit)
+    if (errno == ERANGE || n < 8 || n > 65507 || *endptr != '\0')
+    {
+        fprintf(stderr, "ft_ping: packet size %s is invalid\n", val);
+        return (-1);
+    }
+    conf->opts.packet_size = (uint16_t)n;
+    return (0);
+}
+
+static int parse_timeout(const char *val, t_conf *conf)
+{
+    char *endptr;
+    long n;
+    
+    errno = 0;
+    n = ft_strtol(val, &endptr, 10);
+    if (errno == ERANGE || n < 0 || n > UINT32_MAX || *endptr != '\0')
+    {
+        fprintf(stderr, "ft_ping: bad timeout value\n");
+        return (-1);
+    }
+    conf->opts.timeout = (uint32_t)n;
+    return (0);
+}
+
+static int parse_linger(const char *val, t_conf *conf)
+{
+    char *endptr;
+    long n;
+    
+    errno = 0;
+    n = ft_strtol(val, &endptr, 10);
+    if (errno == ERANGE || n < 0 || n > 3600 || *endptr != '\0')
+    {
+        fprintf(stderr, "ft_ping: bad linger time\n");
+        return (-1);
+    }
+    conf->opts.linger = (uint32_t)n;
+    return (0);
+}
 
 static int  parse_ttl(const char *val, t_conf *conf)
 {
@@ -87,18 +136,27 @@ static int  parse_interval(const char *val, t_conf *conf)
 }
 
 
-static const t_opt_def    opt_table[] = 
+static const t_opt_def opt_table[] = 
 {
-    {"-v", "--verbose",        OPT_NO_ARG, FLAG_VERBOSE, NULL},
-    {"-?", "--help",           OPT_NO_ARG, FLAG_HELP,    NULL},
-    {"-r", "--ignore-routing", OPT_NO_ARG, FLAG_NOROUTE, NULL},
+    {"-v", "--verbose",        false,  FLAG_VERBOSE, NULL},
+    {"-?", "--help",           false,  FLAG_HELP,    NULL},
+    {"-r", "--ignore-routing", false,  FLAG_NOROUTE, NULL},
+    {"-n", "--numeric",        false,  FLAG_NUMERIC, NULL},
+    {"-q", "--quiet",          false,  FLAG_QUIET,   NULL},
+    {"-f", "--flood",          false,  FLAG_FLOOD,   NULL},
+    {"-d", "--debug",          false,  FLAG_DEBUG,   NULL},
     
-    {NULL, "--ttl",        OPT_HAS_ARG, NULL, parse_ttl},
-    {"-c", "--count",      OPT_HAS_ARG, NULL, parse_count},
-    {"-t", "--type",       OPT_HAS_ARG, NULL, parse_type},
-    {"-i", "--interval",   OPT_HAS_ARG, NULL, parse_interval},
+    {"-c", "--count",          true, 0, parse_count},
+    {"-i", "--interval",       true, 0, parse_interval},
+    {"-s", "--size",           true, 0, parse_size},
+    {"-w", "--timeout",        true, 0, parse_timeout},
+    {"-W", "--linger",         true, 0, parse_linger},
+    {"-p", "--pattern",        true, 0, parse_pattern},
+    {"-t", "--type",           true, 0, parse_type},
+    {"-T", "--tos",            true, 0, parse_tos},
+    {NULL, "--ttl",            true, 0, parse_ttl},
     
-    {NULL, NULL, OPT_NO_ARG, NULL, NULL} // Sentinel
+    {NULL, NULL, OPT_NO_ARG, 0, NULL} // Sentinel
 };
 
 static char *get_opt_val(const char *arg)
@@ -139,7 +197,7 @@ static const t_opt_def   *get_opt(const char *arg)
 
 static int consume_opt(const t_opt_def *opt, const char *arg, const char *next_arg, t_conf *conf)
 {
-    if (opt->type == OPT_NO_ARG)
+    if (!opt->has_arg)
     {
         if (opt->flag_bit)
            SET_FLAG(conf, opt->flag_bit); 
